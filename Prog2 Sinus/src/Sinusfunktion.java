0ec7a9b2b0db826c08ef061c42308ee5c195ce55
Sven <flynn23@gmx.de>
2016-10-17 15:32:22 +0200

    JavaFX ausgebessert

--- Prog2 Sinus/src/Sinusfunktion.java
+++ Prog2 Sinus/src/Sinusfunktion.java
@@ -2,10 +2,13 @@ import javafx.application.Application;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
@@ -25,7 +28,7 @@ public class Sinusfunktion extends Application {
 	int i = 0;
 	
 	public void sinus(double f) {
-		if(i > 600)
+		if(i > 900)
 			return;
 		z0 = f * z1 - z2;
 		z2 = z1;
@@ -55,8 +58,8 @@ public class Sinusfunktion extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub	
 		
-		// Textfeld Amplitude für z1
-		Label lAamplitude = new Label("       Amplitude:");
+		// Textfeld Amplitude fuer z1
+		Label lAamplitude = new Label("    Amplitude:");
 		lAamplitude.setPrefHeight(25);
 		TextField tfAmplitude = new TextField ();
 		tfAmplitude.setPrefHeight(25);
@@ -65,7 +68,7 @@ public class Sinusfunktion extends Application {
 		hbAmplitude.setSpacing(10);
 		
 		// Textfeld Frequenz
-		Label lFrequenz = new Label("     Frequenz:");
+		Label lFrequenz = new Label("Frequenz:");
 		lFrequenz.setPrefHeight(25);
 		TextField tfFrequenz = new TextField ();
 		tfFrequenz.setPrefHeight(25);
@@ -73,7 +76,7 @@ public class Sinusfunktion extends Application {
 		hbAmplitude.getChildren().addAll(lFrequenz, tfFrequenz);
 		hbAmplitude.setSpacing(10);
 		
-		// Textfeld b für z2 MUSS NOCH GEMACHT WERDEN
+		// Textfeld b fuer z2 MUSS NOCH GEMACHT WERDEN
 		Label lB = new Label("     b:");
 		lB.setPrefHeight(25);
 		TextField tfB = new TextField ();
@@ -81,19 +84,32 @@ public class Sinusfunktion extends Application {
 		HBox hbB = new HBox();
 		hbAmplitude.getChildren().addAll(lB, tfB);
 		hbAmplitude.setSpacing(10);
+		Button bSend = new Button("Anwenden");
+		VBox vbox = new VBox(8);
+		vbox.getChildren().add(bSend);
+		
+		
+		HBox geschachtelt = new HBox(8);
+		geschachtelt.getChildren().addAll(hbAmplitude, hbFrequenz,hbB, vbox);
+		
+		
 		
 		root = new Group();
 		
-		Scene scene = new Scene(root, 700, 400);
+		Scene scene = new Scene(root, 900, 400);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Scribble");
 		
-		Rectangle panel = new Rectangle(700, 400, Color.WHITESMOKE);
+		Rectangle panel = new Rectangle(900, 400, Color.WHITESMOKE);
 		root.getChildren().add(panel);
-
-		root.getChildren().add(hbAmplitude);
-		root.getChildren().add(hbFrequenz);
-		root.getChildren().add(hbB);
+		
+		
+//		root.getChildren().add(hbAmplitude);
+//		root.getChildren().add(hbFrequenz);
+//		root.getChildren().add(hbB);
+//		root.getChildren().add(vbox);
+		root.getChildren().add(geschachtelt);
+		
 		primaryStage.show();		
 	
 		sinus(1.99);
