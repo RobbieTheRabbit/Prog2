5d8a239944ff0c511a085b63bfdfe78e9d3e9cfc
Sven <flynn23@gmx.de>
2016-10-23 20:16:01 +0200

    Aufgabe 4 fertig

--- Prog2 Sinus/src/vorfuehren.java
+++ Prog2 Sinus/src/vorfuehren.java
@@ -4,34 +4,33 @@ public class vorfuehren {
 
 	public static void main(String[] args){
 		
-
-		zahlen(200);	//	4.1
-//		vorzeichen(35, 10);	//	4.2
-		
-	}
-		
-	//	Wechelt das Vorzeichen bei jedem Durchgang
-	public static void vorzeichen(int zahl, int durchlauf) {
-		int ausgabe = 0;
-		for (int i = 0; i<durchlauf; ++i) {
-			ausgabe = zahl;
-			System.out.print(ausgabe + ", ");
-			ausgabe = zahl - zahl*2; 
-			System.out.print(ausgabe + ", ");
-		}
+//		zahlen(200);	//	4.1
 	}
 
-
-	//	Gibt aus, welche Zahl Gerade und Ungerade ist
+	
+	//	4.1 - Gibt aus, welche Zahl Ungerade ist
 	public static void zahlen(int anzahl) {
-		for (int i = 0; i<anzahl; ++i) {
-			if(i%2 !=0) {
-				System.out.println(i + " ist eine ungerade Zahl");
-			} else{
-				System.out.println(i + " ist eine gerade Zahl");
-			}
-		}	
+			for(int i=1;i<100;i++){
+			System.out.println(x);
+			}	
 	}
+		
+	//	4.2 - Wechelt das Vorzeichen bei jedem Durchgang
+	public static void vorzeichen(int zahl, int durchlauf) {
+		  for(int i=0;i<durchlauf;i++) {
+			   
+			   zahl=zahl-2*zahl;
+		  }
+	}
+
+
+	public static void berechnungFrequenzAmplitude(double beginnAmp, double beginnFreq) {
+
+
+			for (int i = 1; i <= 16; i++) {
+
+
+			}
 	
 	
 	
