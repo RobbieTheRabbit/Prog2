d07823d557d5f90f292854d46aad16d096dbf2f9
Sagitova Gulnaz <s58888@beuth-hochschule.de>
2016-10-31 15:24:11 +0100

    skellet Klassen für die Aufgabe 3

---
+++ Prog2 Sinus/src/uebung2/rekursionSchneeflocke.java
@@ -0,0 +1,16 @@
+package uebung2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class rekursionSchneeflocke extends Application{
+	
+	public static void main(String[] args){
+		launch(args);
+	}
+
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+	}}
