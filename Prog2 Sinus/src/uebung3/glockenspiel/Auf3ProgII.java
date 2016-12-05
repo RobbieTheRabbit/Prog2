88f0b450c5e3ef905aeec6542a0260f2c02720ca
Sagitova Gulnaz <s58888@beuth-hochschule.de>
2016-12-05 14:39:55 +0100

    fehlerbehebung in eclipse

--- Prog2 Sinus/src/uebung3/glockenspiel/Auf3ProgII.java
+++ Prog2 Sinus/src/uebung3/glockenspiel/Auf3ProgII.java
@@ -20,7 +20,7 @@ public class Auf3ProgII extends Application {
              
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/uebung3/glockenspiel/FXMLDocument.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
